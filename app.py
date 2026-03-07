33b0cb398c6434935744c0ae63caae0ff43c8ba2
Saurabh Chandra Baij <saurabhbaij@gmail.com>
2026-03-07 15:12:12 +0530

    fsas

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, jsonify, request
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_comment_downloader import YoutubeCommentDownloader
 from youtube_transcript_api.proxies import GenericProxyConfig
+import os
 
 app = Flask(__name__)
 
-PROXY_URL = "http://pc3a9JA3tG-res-any:PC_2NoKG65deJmddoZHE@proxy-us.proxy-cheap.com:59593"
+PROXY_URL = "http://pc3a9JA3tG-res-any:PC_2NoKG65deJmddoZHE@proxy-us.proxy-cheap.com:5959"
 
 @app.route('/')
 def home():
