bc176906a9b1e09a6a5cb1ed2ce88df41d73a7b7
Saurabh Chandra Baij <saurabhbaij@gmail.com>
2026-03-07 15:03:29 +0530

    sasad

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_comment_downloader import YoutubeCommentDownloader
+from youtube_transcript_api.proxies import GenericProxyConfig
 
 app = Flask(__name__)
+
+PROXY_URL = "http://pc3a9JA3tG-res-any:PC_2NoKG65deJmddoZHE@proxy-us.proxy-cheap.com:59593"
 
 @app.route('/')
 def home():
@@ -14,7 +17,12 @@ def get_transcript():
     if not video_id:
         return jsonify({"error": "Please provide a video id"}), 400
     try:
-        ytt = YouTubeTranscriptApi()
+        ytt = YouTubeTranscriptApi(
+            proxy_config=GenericProxyConfig(
+                http_url=PROXY_URL,
+                https_url=PROXY_URL,
+            )
+        )
         transcript = ytt.fetch(video_id)
         return jsonify([{"text": s.text, "start": s.start} for s in transcript])
     except Exception as e:
